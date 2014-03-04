ea1f99646fbe75b26bef87d0d9e1e12566c802bc
Joshua <joshua.hardenbrook@cern.ch>
2014-03-04 13:24:23 +0100

    added signal efficiency study

---
+++ SIGNAL_EFFICIENCY/signal_eff.py
@@ -0,0 +1,7 @@
+import ROOT as rt
+import rootlogon
+
+pileup20x25 = rt.TFile("/afs/cern.ch/work/h/hardenbr/2014/HIGGS_DIPHOTON_HLT/res/tree.root")
+pileup40x25 = rt.TFile("/afs/cern.ch/work/h/hardenbr/2014/HIGGS_DIPHOTON_HLT/res/tree.root")
+pileup40x50 = rt.TFile("/afs/cern.ch/work/h/hardenbr/2014/HIGGS_DIPHOTON_HLT/res/tree.root")
+pileup80x50 = rt.TFile("/afs/cern.ch/work/h/hardenbr/2014/HIGGS_DIPHOTON_HLT/res/tree.root")
